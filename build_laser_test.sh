171adb7d0540f2d3948e68beed51c7ddb8658219
Doridian <git@doridian.net>
2026-02-06 20:25:01 -0800

    Fix build of test

--- build_laser_test.sh
+++ build_laser_test.sh
@@ -6,7 +6,7 @@ set -u
 gcc graphics.c -o graphics.o -c -Werror
 gcc laser.c -o laser.o -c -Werror
 gcc laser_test.c -o laser_test.o -c -Werror
-gcc -o laser_test laser_test.o graphics.o laser.o -Werror
+gcc -lm -o laser_test laser_test.o graphics.o laser.o -Werror
 
 rm *.o
 
